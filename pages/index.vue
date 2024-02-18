b016c46e3363e079a3623cd8dbc06ad2ded98675
Cristina <avicon95@protonmail.com>
2024-02-18 18:00:05 -0500

    chore: add Hiruko font and add main title

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,21 @@
     class="bg-background dark:bg-background-dark text-foreground dark:text-foreground-dark"
   >
     <div class="container mx-auto px-20 grid grid-cols-2 h-screen">
-      <div class="overflow-hidden">
-        <p class="h-screen py-10">Left side</p>
+      <div class="overflow-hidden py-20">
+        <h1 class="font-[Hiruko] text-7xl">Cristina Vidal</h1>
+        <h2 class="font-[Hiruko] text-4xl">Software Developer</h2>
+        <svg
+          width="496"
+          height="10"
+          viewBox="0 0 496 10"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          class="fill-highlight dark:fill-highlight-dark mt-2"
+        >
+          <rect y="1" width="450" height="8" rx="4" />
+          <rect x="456" y="1" width="24" height="8" rx="4" />
+          <circle cx="491" cy="5" r="5" />
+        </svg>
       </div>
       <div
         class="bg-red-600 overflow-y-auto scrollbar-hidden scroll-smooth py-10"
--- pages/test.vue
+++ pages/test.vue
@@ -4,6 +4,8 @@
     <option value="light">Light</option>
     <option value="dark">Dark</option>
   </select>
+  <h1>This is my title</h1>
+  <h2 class="font-[Hiruko]">Another one</h2>
   <div
     class="h-20 bg-background dark:bg-background-dark text-foreground dark:text-foreground-dark"
   >
