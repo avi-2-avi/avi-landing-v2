f929015b055add7055366f56bf53f4a83dba3634
Cristina <avicon95@protonmail.com>
2024-02-19 08:57:24 -0500

    chore: readjust title part size

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,18 @@
   <div
     class="bg-background dark:bg-background-dark text-foreground dark:text-foreground-dark"
   >
-    <div class="container mx-auto px-20 grid grid-cols-2 h-screen">
+    <div
+      class="container mx-auto px-20 grid grid-cols-1 lg:grid-cols-2 h-screen"
+    >
       <div class="overflow-hidden py-20">
-        <h1 class="font-[Hiruko] text-7xl">Cristina Vidal</h1>
-        <h2 class="font-[Hiruko] text-4xl">Software Developer</h2>
+        <h1 class="font-[Hiruko] text-5xl 2xl:text-6xl">Cristina Vidal</h1>
+        <h2 class="font-[Hiruko] text-2xl 2xl:text-3xl">Software Developer</h2>
         <svg
-          width="496"
           height="10"
           viewBox="0 0 496 10"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
-          class="fill-highlight dark:fill-highlight-dark mt-2"
+          class="fill-highlight dark:fill-highlight-dark mt-2 w-[400px] 2xl:w-[496px]"
         >
           <rect y="1" width="450" height="8" rx="4" />
           <rect x="456" y="1" width="24" height="8" rx="4" />
@@ -20,7 +21,7 @@
         </svg>
       </div>
       <div
-        class="bg-red-600 overflow-y-auto scrollbar-hidden scroll-smooth py-10"
+        class="bg-red-600 overflow-y-auto scrollbar-hidden scroll-smooth py-20"
       >
         <p>Right side</p>
       </div>
